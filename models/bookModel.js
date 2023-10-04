2b23ec885d3333afe4dd0ba9305faa621f66e411
MannShah17 <mannshah2004@gmail.com>
2023-10-04 15:50:53 +0530

    boilerplate

--- models/bookModel.js
+++ models/bookModel.js
@@ -1,14 +1,35 @@
-const mongoose = require('mongoose');
-
-// Define the weather data schema
-const weatherDataSchema = new mongoose.Schema({
-  // cityName where the weather data is recorded (String, Required)
-  // temperature in degrees Celsius (Number, Required)
-  // zipCode for the location (String, Required)
-  // humidity percentage (Number, Required)
-  // weatherDescription (e.g., "Partly Cloudy") (String, Required)
+// Define the Book Schema
+const bookSchema = new mongoose.Schema({
+  // 1)   {
+  // // Define the 'title' field
+  // // The data type of this field is a string
+  // // This field is required
+  // }
+  // 2) {
+  // // Define the 'author' field
+  // // The data type of this field is a string
+  // // This field is required
+  // }
+  // 3) {
+  // // Define the 'ISBN' field
+  // // The data type of this field is a string
+  // // This field is required
+  // // Each ISBN must be unique
+  // }
+  // 4) {
+  //   // Define the 'publishedYear' field
+  //   // The data type of this field is a number
+  //   // This field is required
+  // }
+  // 5) Define the 'genre' field
+  // The data type of this field is a string
+  // 6) {
+  //   // Define the 'copiesAvailable' field
+  //   // The data type of this field is a number
+  //  // The default value is 1
+  // }
 });
 
-// Create a model for WeatherData using the schema
-
-module.exports = WeatherData;
+// Create and export the Book model
+const Book = mongoose.model('Book', bookSchema);
+module.exports = Book;
